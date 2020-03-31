ee60189deb939887a44184a2ffd024710cb3d0b0
com <com@DESKTOP-1IGND1M>
2020-04-01 04:19:49 +0900

    Firebase connection

--- src/DB_Monitoring_Service/Main.java
+++ src/DB_Monitoring_Service/Main.java
@@ -1,20 +1,105 @@
 package DB_Monitoring_Service;
 
-import java.text.SimpleDateFormat;
+
 import java.util.ArrayList;
-import java.util.Date;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
 
 import DB_Monitoring_Service.Custom_Data_Type.DeviceInfo;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import com.google.api.core.ApiFuture;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.FirestoreOptions;
+import com.google.cloud.firestore.WriteResult;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.FirestoreClient;
+
 
 
 public class Main{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
+	
+		
+		// Use a service account
+		InputStream serviceAccount = new FileInputStream("service-account-file.json");
+		GoogleCredentials credentials = GoogleCredentials.fromStream(serviceAccount);
+		FirebaseOptions FBoptions = new FirebaseOptions.Builder()
+		    .setCredentials(credentials)
+		    .build();
+		FirebaseApp.initializeApp(FBoptions);
+
+		Firestore db = FirestoreClient.getFirestore();
+
+
+		// Create a new user with a first and last name
+		Map<String, Object> user = new HashMap<>();
+		user.put("first", "Ada");
+		user.put("last", "Lovelace");
+		user.put("born", 1815);
+
+		/*
+		DocumentReference docRef = db.collection("users").document("alovelace");
+		// Add document data  with id "alovelace" using a hashmap
+		Map<String, Object> data = new HashMap<>();
+		data.put("first", "Ada");
+		data.put("last", "Lovelace");
+		data.put("born", 1815);
+		//asynchronously write data
+		ApiFuture<WriteResult> result = docRef.set(data);
+		// ...
+		// result.get() blocks on response
+		try {
+			System.out.println("Update time : " + result.get().getUpdateTime());
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		*/
+		
+		
+		DocumentReference docRef = db.collection("users").document("aturing");
+		// Add document data with an additional field ("middle")
+		Map<String, Object> data = new HashMap<>();
+		data.put("first", "Alan");
+		data.put("middle", "Mathison");
+		data.put("last", "Turing");
+		data.put("born", 1912);
+
+		ApiFuture<WriteResult> result = docRef.set(data);
+		try {
+			System.out.println("Update time : " + result.get().getUpdateTime());
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		System.out.print("test");
+		
 //		SingleThreadMode();
-		MutiThreadMode();	
+		// MutiThreadMode();	
 	}
 	
 	
